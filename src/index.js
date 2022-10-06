934dc3914bbfcb10a6cab0694cb77bf1e29c5c8e
Joontae93 <kj.roelke@gmail.com>
2022-10-06 11:07:24 -0500

    clear the console

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import { scrollytelling } from './modules/scrolly';
 import { myCopyright } from './modules/utilities';
 
 function init() {
-	console.log(`Let's get to building!`);
 	myCopyright();
 	scrollytelling();
 }
--- src/modules/scrolly.js
+++ src/modules/scrolly.js
@@ -1,12 +1,17 @@
 import scrollama from 'scrollama';
 import { select } from './utilities';
+
 export function scrollytelling() {
+	document.addEventListener('click', (ev) => {
+		console.log('Click!');
+	});
+}
+function prayerWalk() {
 	const scroller = scrollama();
 	scroller.setup({ step: '.station', offset: 0.5 });
 	scroller.onStepEnter(displayStation);
 }
 function displayStation(data) {
-	console.log(data);
 	const stations = select('.station', true);
 	const image = select('.image');
 	stations.forEach((el, i) => {
