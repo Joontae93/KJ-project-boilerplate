74e83cbea2c1aab981873a175c5ae8a64f952272
KJ Roelke <kj.roelke@gmail.com>
2022-10-06 15:49:33 -0500

    add transforms, images

--- src/modules/scrolly.js
+++ src/modules/scrolly.js
@@ -5,9 +5,11 @@ export function scrollytelling() {
 	const main = select('main');
 	const button = select('.btn');
 	main.style.opacity = 0;
+	main.style.display = 'none';
 	document.addEventListener('click', (ev) => {
 		if (ev.target != button) return;
 		main.style.opacity = 1;
+		main.style.display = 'grid';
 		button.style.opacity = 0;
 		button.style.display = 'none';
 		prayerWalk();
@@ -15,16 +17,36 @@ export function scrollytelling() {
 }
 function prayerWalk() {
 	const scroller = scrollama();
-	scroller.setup({ step: '.station', offset: 0.5 });
-	scroller.onStepEnter(displayStation);
+	scroller.setup({
+		step: '.station',
+		offset: 0.5,
+		progress: true,
+		threshold: 10,
+	});
+	scroller.onStepProgress(displayStation);
+	scroller.onStepExit(hideStation);
 }
 function displayStation(data) {
+	const stations = select('.station', true);
+	const images = select('.image', true);
+	images.forEach((el) => (el.style.opacity = 0));
+	stations.forEach((el, i) => {
+		if (i === data.index) {
+			const image = images[i];
+			image.style.opacity = 1;
+			image.style.transform = `rotateY(${
+				(parseInt(data.progress * 100) - 30) * 0.4
+			}deg)`;
+		}
+	});
+}
+
+function hideStation(data) {
 	const stations = select('.station', true);
 	const image = select('.image');
 	stations.forEach((el, i) => {
 		if (i === data.index) {
-			const title = el.querySelector('.station__title').innerText;
-			image.innerHTML = title;
+			image.style.opacity = 0;
 		}
 	});
 }
